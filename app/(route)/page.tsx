ee279cd7367b1e1cce5b504b76dac002248ea18d
Priyanshu255 <priyanshupanditlg12a@gmail.com>
2024-08-16 14:16:45 +0530

    implement stripe

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,8 +1,8 @@
 // import CategoryFilter from '@/components/shared/CategoryFilter';
-// import Collection from '@/components/shared/Collection'
+import Collection from '@/components/shared/Collection'
 // import Search from '@/components/shared/Search';
 import { Button } from '@/components/ui/button'
-// import { getAllEvents } from '@/lib/actions/event.actions';
+import { getAllEvents } from '@/lib/actions/event.actions';
 import { SearchParamProps } from '@/types';
 import Image from 'next/image'
 import Link from 'next/link'
@@ -12,16 +12,16 @@ export default async function Home({ searchParams }: SearchParamProps) {
   const searchText = (searchParams?.query as string) || '';
   const category = (searchParams?.category as string) || '';
 
-  // const events = await getAllEvents({
-  //   query: searchText,
-  //   category,
-  //   page,
-  //   limit: 6
-  // })
+  const events = await getAllEvents({
+    query: searchText,
+    category,
+    page,
+    limit: 6
+  })
 
   return (
     <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10 px-20">
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10 md:px-20">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
             <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
@@ -43,7 +43,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
         </div>
       </section> 
 
-      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12 px-20">
+      <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12 md:px-20">
         <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
@@ -51,7 +51,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           {/* <CategoryFilter /> */}
         </div>
 
-        {/* <Collection 
+        <Collection 
           data={events?.data}
           emptyTitle="No Events Found"
           emptyStateSubtext="Come back later"
@@ -59,7 +59,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
           limit={6}
           page={page}
           totalPages={events?.totalPages}
-        /> */}
+        />
       </section>
     </>
   )
