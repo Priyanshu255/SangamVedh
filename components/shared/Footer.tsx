e19b79bd9e911b196a98024b6d293c54c903ce7e
Priyanshu255 <priyanshupanditlg12a@gmail.com>
2024-12-17 23:42:51 +0530

    name correct

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -6,15 +6,16 @@ const Footer = () => {
     <footer className="border-t px-10">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href='/'>
-          <Image 
+          {/* <Image 
             src="/assets/images/logo.svg"
             alt="logo"
             width={128}
             height={38}
-          />
+          /> */}
+          <h1 className="font-extrabold text-2xl">SangamVedh</h1>
         </Link>
 
-        <p>2023 Evently. All Rights reserved.</p>
+        <p>&#169; 2024 SangamVedh. All Rights reserved.</p>
       </div>
     </footer>
   )
